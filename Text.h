0751081f52f74124a4782e5496628da0d8ba6ef3
KordianRybarczyk <250993@edu.p.lodz.pl>
2024-05-23 13:07:13 +0200

    Stworzono Łańcuch tekstowy

---
+++ Text.h
@@ -0,0 +1,11 @@
+
+
+#ifndef TEXTFINAL_TEXT_H
+#define TEXTFINAL_TEXT_H
+void enterText(char *text);
+void printText(char *text);
+void clear(void);
+int count(char *text);
+void bigLetters(char *string);
+void smallLetters(char *string);
+#endif //TEXTFINAL_TEXT_H
